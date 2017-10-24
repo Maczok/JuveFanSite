0015f2718d06442c3ad2411f36b51850d90f64c3
Maczok <maciej.duda1@gazeta.pl>
2017-10-24 11:09:18 +0200

    gruntfile config: browsersync z automatycznym reloadem na zmianę. Przerobiłem css na scss aby wszystko ładnie wyglądało w testach. W html zmieniłem style.css na ./css/style.css

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,44 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+// Project configuration.
+
+grunt.initConfig({
+  browserSync: {
+    dev: {
+     bsFiles: {
+        src : [
+            'css/*.css',
+            './*.html'
+            ]
+     },
+     options: {
+        watchTask: true,
+        server: './'
+     }
+    }
+  },
+   
   sass: {
-      options: {
-        sourceMap: true
+    options: {
+      sourceMap: true
       },
-      dist: {
-        files: {
-          'css/style.css': 'scss/style.scss'
-        }
+    dist: {
+      files: {
+        'css/style.css': 'scss/style.scss'
       }
-    },
-    imagemin: {
-  		dynamic: {
-  			files: [{
-  				expand: true,
-  				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
-  				dest: 'images/build/'
-  			}]
-  		}
-  	},
+    }
+  },
+
+  imagemin: {
+    dynamic: {
+  		files: [{
+  			expand: true,
+  			cwd: 'images/',
+  			src: ['**/*.{png,jpg,gif}'],
+  			dest: 'images/build/'
+  		}]
+  	}
+  },
 	watch: {
     		scripts: {
         		files: ['scss/*.scss'],
@@ -33,9 +50,11 @@ module.exports = function(grunt) {
 	}
   });
   // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-serve');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'imagemin','watch']);
+  grunt.registerTask('default', ['browserSync','sass', 'imagemin','watch']);
 };
